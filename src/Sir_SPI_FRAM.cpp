8818f4b37fd18048b272b88594b7b5f54d185742
Imran Paruk <imranparuk@live.com>
2020-01-26 00:07:34 +0200

    Add all code SPI FRAM

--- src/Sir_SPI_FRAM.cpp
+++ src/Sir_SPI_FRAM.cpp
@@ -6,11 +6,10 @@
 
 Sir_SPI_FRAM::Sir_SPI_FRAM(int8_t cs, SPIClass *spi) {
   _cs = cs;
-  _initialized = false;
   *_spi = *spi;
 }
 
-boolean Sir_SPI_FRAM::init(uint8_t sizeAddress) {
+boolean Sir_SPI_FRAM::init(uint8_t sizeAddress, unit16_t operatingFreq) {
   if (_cs == -1) {
     return false;
   }
@@ -21,20 +20,148 @@ boolean Sir_SPI_FRAM::init(uint8_t sizeAddress) {
   digitalWrite(_cs, HIGH);
 
   _spi->begin();
-  spiSettings = SPISettings(20000000, MSBFIRST, SPI_MODE0); 
+  spiSettings = SPISettings(operatingFreq, MSBFIRST, SPI_MODE0); 
 
   uint8_t manufID;
   uint16_t prodID;
   getDeviceID(&manufID, &prodID);
 
-  if (manufID != 0x04 && manufID != 0x7f) {
+  if (manufID != MAN_ID_0 && manufID != MAN_ID_1) {
     return false;
   }
-  if (prodID != 0x0302 && prodID != 0x7f7f) {
+  if (prodID != PROD_ID_0 && prodID != PROD_ID_1) {
     return false;
   }
-
-  _initialized = true;
 
   return true;
 }
+
+uint8_t Sir_SPI_FRAM::getAddressSize() {
+  return _nAddressSizeBytes;
+}
+
+void Sir_SPI_FRAM::writeAddress(uint32_t addr) {
+  if (_nAddressSizeBytes > 3)
+      _spi->transfer((uint8_t)(addr >> 24));
+  if (_nAddressSizeBytes > 2)
+      _spi->transfer((uint8_t)(addr >> 16));
+  _spi->transfer((uint8_t)(addr >> 8));
+  _spi->transfer((uint8_t)(addr & 0xFF));
+}
+
+
+void Sir_SPI_FRAM::writeByte(uint32_t addr, uint8_t value) {
+  _spi->beginTransaction(spiSettings); 
+  digitalWrite(_cs, LOW);
+  _spi->transfer(WRITE);
+  writeAddress(addr);
+  _spi->transfer(value);
+  digitalWrite(_cs, HIGH);
+  _spi->endTransaction(); 
+}
+
+uint8_t Sir_SPI_FRAM::readByte(uint32_t addr) {
+  _spi->beginTransaction(spiSettings); 
+  digitalWrite(_cs, LOW);
+  _spi->transfer(READ);
+  writeAddress(addr);
+  uint8_t ret = _spi->transfer(0);
+  digitalWrite(_cs, HIGH);
+  _spi->endTransaction(); 
+  return ret;
+}
+
+void Sir_SPI_FRAM::writeEnable(bool enable) {
+  _spi->beginTransaction(spiSettings); 
+  digitalWrite(_cs, LOW);
+
+  uint8_t opcode;
+  if (enable) opcode = WREN 
+  else opcode = WRDI
+  _spi->transfer(opcode);
+
+  digitalWrite(_cs, HIGH);
+  _spi->endTransaction(); 
+}
+
+void Sir_SPI_FRAM::write(uint32_t addr, const uint8_t *values, size_t count) {
+  _spi->beginTransaction(spiSettings); 
+  digitalWrite(_cs, LOW);
+
+  _spi->transfer(WRITE);
+  writeAddress(addr);
+  for (size_t i = 0; i < count; i++) {
+    _spi->transfer(values[i]);
+  }
+  
+  digitalWrite(_cs, HIGH);
+  _spi->endTransaction(); 
+}
+
+void Sir_SPI_FRAM::read(uint32_t addr, uint8_t *values, size_t count) {
+  _spi->beginTransaction(spiSettings); 
+  digitalWrite(_cs, LOW);
+
+  _spi->transfer(READ);
+  writeAddress(addr);
+  for (size_t i = 0; i < count; i++) {
+    uint8_t x = _spi->transfer(0);
+    values[i] = x;
+  }
+
+  digitalWrite(_cs, HIGH);
+  _spi->endTransaction(); 
+}
+
+uint8_t Sir_SPI_FRAM::getStatus() {
+  uint8_t x = 0;
+
+  _spi->beginTransaction(spiSettings); 
+  digitalWrite(_cs, LOW);
+
+  _spi->transfer(RDSR);
+  x = _spi->transfer(0);
+
+  digitalWrite(_cs, HIGH);
+  _spi->endTransaction(); 
+
+  return x;
+}
+
+void Sir_SPI_FRAM::setStatus(uint8_t val) {
+  _spi->beginTransaction(spiSettings); 
+  digitalWrite(_cs, LOW);
+
+  _spi->transfer(WRSR);
+  _spi->transfer(val);
+
+  digitalWrite(_cs, HIGH);
+  _spi->endTransaction(); 
+}
+
+void Sir_SPI_FRAM::getDeviceID(uint8_t *manufacturerID, uint16_t *productID) {
+  uint8_t temp[4] = {0, 0, 0, 0};
+
+  _spi->beginTransaction(spiSettings); 
+  digitalWrite(_cs, LOW);
+  _spi->transfer(RDID);
+
+  temp[0] = SPItransfer(0);
+  temp[1] = SPItransfer(0);
+  temp[2] = SPItransfer(0);
+  temp[3] = SPItransfer(0);
+
+  digitalWrite(_cs, HIGH);
+  _spi->endTransaction(); 
+
+  *manufacturerID = (a[0]);
+  *productID = (a[2] << 8) + a[3];
+}
+
+void Sir_SPI_FRAM::erase(uint32_t maxAddr) {
+  uint32_t i = 0;
+  while (i < maxAddress) {
+    writeByte(i++, 0);
+  }
+}
+
--- src/Sir_SPI_FRAM.h
+++ src/Sir_SPI_FRAM.h
@@ -4,20 +4,62 @@
 #include <Arduino.h>
 #include <SPI.h>
 
- #DEFINE WREN_CMD 0b0110  
- #DEFINE WRDI_CMD 0b0100  
- #DEFINE RDSR_CMD 0b0101  
- #DEFINE WRSR_CMD 0b0001  
- #DEFINE RDID_CMD 0b10011111  
- #DEFINE READ_CMD 0b0011  
- #DEFINE WRITE_CMD 0b0010  
+// Status register
+#define WPEN       0x80
+#define BP1        0x08
+#define BP0        0x04
+#define WEL        0x02
+
+// OP-CODE
+#define WREN       0x06
+#define WRDI       0x04
+#define RDSR       0x05
+#define WRSR       0x01
+#define READ       0x03
+#define WRITE      0x02
+#define RDID       0x9F
+#define SLEEP      0xB9
+
+#define MAN_ID_0   0x04
+#define MAN_ID_1   0x7f
+
+#define PROD_ID_0  0x0302
+#define PROD_ID_1  0x7f7f
+
 
 class Sir_SPI_FRAM {
 private:
+  uint8_t _nAddressSizeBytes, _cs;
+  SPIClass *_spi;
+  SPISettings spiSettings;
 
 public:
-  Sir_SPI_FRAM();
+  Sir_SPI_FRAM(int8_t, SPIClass *);
 
-  boolean init(uint8_t);
+  boolean init(uint8_t, unit16_t);
 
+  uint8_t getStatus(void);
+  void setStatus(uint8_t);
+
+  void writeByte();
+  uint8_t readByte();
+
+  void setStatus(uint8_t);
+  uint8_t getStatus();
+
+  void write(uint32_t, const uint8_t *, size_t);
+  void read(uint32_t, const uint8_t *, size_t);
+
+  void writeEnable(bool);
+
+  uint8_t readByte(uint32_t);
+  void writeByte(uint32_t, uint8_t);
+
+  void writeAddress(uint32_t);
+
+  uint8_t getAddressSize(); 
+
+  void getDeviceID(uint8_t *, uint16_t *);
+
+  void erase();
 };
